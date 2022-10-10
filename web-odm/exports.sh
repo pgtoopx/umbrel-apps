76e92540517043ec7f1c6c842487407bc2efbcfb
pgtoopx <eduardoinbrand@gmail.com>
2022-10-10 01:54:12 +0000

    add export

---
+++ web-odm/exports.sh
@@ -0,0 +1,11 @@
+export WO_HOST=localhost
+export WO_PORT=8000
+export WO_MEDIA_DIR=appmedia
+export WO_SSL=NO
+export WO_SSL_KEY=
+export WO_SSL_CERT=
+export WO_SSL_INSECURE_PORT_REDIRECT=80
+export WO_DEBUG=NO
+export WO_DEV=NO
+export WO_BROKER=redis://broker
+export WO_DEFAULT_NODES=1
